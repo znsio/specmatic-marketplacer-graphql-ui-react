ea44b444ad19e5b0f1ae7e8c355253ae55d85791
Joel Rosario <cirquitz@gmail.com>
2024-06-16 23:17:42 +0200

    Read the test data from the test data file

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -6,8 +6,26 @@ import client from "../apolloClient";
 import CartForm from "../components/CartForm";
 import { startGraphQlStub, stopGraphQlStub } from "specmatic";
 
+const fs = require('fs');
+const path = require('path');
+
 global.setImmediate = global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
 let stub;
+
+function readCartValues() {
+  console.log(__dirname);
+  console.log(path.resolve(__dirname, '../../test_data/createCart.md'))
+  const data = fs.readFileSync(path.resolve(__dirname, '../../test_data/createCart.md'), 'utf-8');
+  const firstNameMatch = data.match(/firstName: "([^"]+)"/);
+  const surnameMatch = data.match(/surname: "([^"]+)"/);
+  const phoneMatch = data.match(/phone: "([^"]+)"/);
+
+  return {
+    firstName: firstNameMatch ? firstNameMatch[1] : '',
+    surname: surnameMatch ? surnameMatch[1] : '',
+    phone: phoneMatch ? phoneMatch[1] : ''
+  };
+}
 
 beforeAll(async () => {
   stub = await startGraphQlStub("127.0.0.1", 8080, "./test_data");
@@ -23,8 +41,9 @@ jest.mock("react-toastify", () => ({
 }));
 
 describe("App component tests", () => {
-  // Test for the create cart form
   test('creates a cart and displays cart details', async () => {
+    const { firstName, surname, phone } = readCartValues();
+
     render(
       <ApolloProvider client={client}>
         <CartForm />
@@ -38,13 +57,15 @@ describe("App component tests", () => {
     const cartDetails = await screen.findByTestId('cartDetails');
 
     expect(cartDetails).toBeInTheDocument();
-    expect(screen.getByTestId('firstName')).toHaveValue('John');
-    expect(screen.getByTestId('surname')).toHaveValue('Doe');
-    expect(screen.getByTestId('phone')).toHaveValue('1234567890');
+    expect(screen.getByTestId('firstName')).toHaveValue(firstName);
+    expect(screen.getByTestId('surname')).toHaveValue(surname);
+    expect(screen.getByTestId('phone')).toHaveValue(phone);
   });
 
   // Test for the fetch cart form
   test('fetches a cart by ID and displays cart details', async () => {
+    const { firstName, surname, phone } = readCartValues();
+
     render(
       <ApolloProvider client={client}>
         <CartForm />
@@ -58,10 +79,11 @@ describe("App component tests", () => {
     const fetchedCartDetails = await screen.findByTestId('fetchedCartDetails');
 
     expect(fetchedCartDetails).toBeInTheDocument();
-    expect(screen.getByTestId('firstName')).toHaveValue('John');
-    expect(screen.getByTestId('surname')).toHaveValue('Doe');
-    expect(screen.getByTestId('phone')).toHaveValue('1234567890');
+    expect(screen.getByTestId('firstName')).toHaveValue(firstName);
+    expect(screen.getByTestId('surname')).toHaveValue(surname);
+    expect(screen.getByTestId('phone')).toHaveValue(phone);
   });
+
 });
 
 afterAll(async () => {
